0a9cba5b39df6aaa5a70e2152c3d018caa33bae8
Jonathan Maw <jonathan.maw@codethink.co.uk>
2012-02-21 17:50:47 +0000

    Summary extracts USS, GSS, SSS and the rest from smaps, and presents it together.

--- useproc.c
+++ useproc.c
@@ -24,13 +24,23 @@
 					  *(page size = 1<<page shift)*/
 #define PAGESHIFT 55 /*The number of bits that need to be shifted down to
 		      *express the pageshift as a number*/
+#define KBSIZE 1024 /*The number of bytes in a kilobyte*/
 #define DEBUG 1
 
 struct sizestats{
-	uint uss; /*Count of pages unique to the process (only mapped once)*/
-	uint sss; /*Count of pages only mapped within the process*/
-	uint gss; /*Count of pages only mapped by the processes specified as
-		   *arguments to this program*/
+	uint vss; /*Amount of memory kB addressed in memory map*/
+	uint rss; /*Amount of memory kB memory-mapped in RAM*/
+	uint pss; /*Amount of memory kB per page divided by number of 
+		   *times mapped*/
+	uint uss; /*Amount of memory kB unique to the process 
+		   *(only mapped once)*/
+	uint sss; /*Amount of memory kB only mapped within the process*/
+	uint gss; /*Amount of memory kB only mapped by the processes
+	 	   *specified as arguments to this program*/
+	uint refd;/*Amount of memory kB in pages referenced*/
+	uint swap;/*Amount of memory kB mapped to a swap file/partition*/
+	uint anon;/*Amount of memory kB without an associated file*/
+	uint locked;/*Amount of memory kB locked*/
 };
 struct pagedata {
 	uint memmapped; /*Internal store of how many times this page has
@@ -69,26 +79,78 @@ void cleanup(int signal)
 
 void printsummary(struct sizestats *stats)
 {
-	printf("The number of pages used only once in this process"
-			" (USS): %d\n", stats->uss);
-	printf("The number of pages used only by this process"
-			" (SSS): %d\n", stats->sss);
+	printf( "Vss: \t\t %8u kB\n"
+		"Rss: \t\t %8u kB\n"
+		"Pss: \t\t %8u kB\n"
+		"Uss: \t\t %8u kB\n"
+		"Sss: \t\t %8u kB\n"
+		"Gss: \t\t %8u kB\n"
+		"Referenced: \t %8u kB\n"
+		"Swap: \t\t %8u kB\n"
+		"Anonymous: \t %8u kB\n"
+		"Locked: \t %8u kB\n",
+		stats->vss, stats->rss, stats->pss, stats->uss, stats->sss,
+		stats->gss, stats->refd, stats->swap, stats->anon,
+		stats->locked);
+}
+
+void parse_smaps_file(FILE *file, struct sizestats *stats)
+{
+	int ret = 0;
+	int temp=0;
+	char buffer[BUFFER_SIZE];
+	char* pt = buffer;
+	size_t n = 0;
+	ret = getline(&pt, &n, file);
+
+	while(ret > 0){
+	getdelim(&pt, &n, ':', file);
+	fscanf(file,"%d kB\n", &temp);
+		stats->vss += temp;
+	getdelim(&pt, &n, ':', file);
+	fscanf(file,"%d kB\n", &temp);
+		stats->rss += temp;
+	getdelim(&pt, &n, ':', file);
+	fscanf(file,"%d kB\n", &temp);
+		stats->pss += temp;
+	getdelim(&pt, &n, ':', file);
+	getdelim(&pt, &n, ':', file);
+	getdelim(&pt, &n, ':', file);
+	getdelim(&pt, &n, ':', file);
+	getdelim(&pt, &n, ':', file);
+	fscanf(file,"%d kB\n", &temp);
+		stats->refd += temp;
+	getdelim(&pt, &n, ':', file);
+	fscanf(file,"%d kB\n", &temp);
+		stats->anon += temp;
+	getdelim(&pt, &n, ':', file);
+	fscanf(file,"%d kB\n", &temp);
+		stats->swap += temp;
+	getdelim(&pt, &n, ':', file);
+	getdelim(&pt, &n, ':', file);
+	getdelim(&pt, &n, ':', file);
+	fscanf(file,"%d kB\n", &temp);
+		stats->locked += temp;
+	ret = getline(&pt, &n, file);
+	}
 }
 
 void countgss(void *key, void *value, void *data)
 {
+	size_t pagesize = getpagesize();
 	struct pagedata *page = value;
 	struct sizestats *stats = data;
 	if (page->memmapped == page->procmapped)
-		stats->gss += 1;
+		stats->gss += pagesize;
 }
 
 void countsss(void *key, void *value, void *data)
 {
+	size_t pagesize = getpagesize();
 	struct pagedata *page = value;
 	struct sizestats *stats = data;
 	if (page->memmapped == page->procmapped)
-		stats->sss += 1;
+		stats->sss += pagesize;
 }
 
 void handle_errno(const char *context)
@@ -103,18 +165,68 @@ strerror(errno));
 
 void lookup_addresses(const char* buffer, int fd, GHashTable *pages)
 {
-	/*parse string into addresses and look them up in pagemap (fd)
-	 *get the PFN (if available) and lookup in hashtable pages.*/
+/*parse string into addresses and look them up in pagemap (fd)
+ *get the PFN (if available) and lookup in hashtable pages.*/
+	uint32_t addrstart;
+	uint32_t addrend;
+	uint32_t address;
+	uint64_t bitbuffer;
+	int retval;
+	size_t pagesize = getpagesize();
+	size_t stepsize = sizeof(uint64_t);
+
+	retval = sscanf(buffer, "%x-%x", &addrstart, &addrend);
+	handle_errno("parsing addresses from a line of maps file");
+	if (retval < 2){
+		fprintf(stderr, "Error, could not find both address "
+			"start and address end\n");
+		exit(EXIT_FAILURE);
+	}
+	if (((addrend - addrstart) / pagesize) < 1){
+		fprintf(stderr, "Error: VMA contains no pages\n");
+		exit(EXIT_FAILURE);
+	}
+
+	addrstart /= (pagesize / stepsize);
+	addrend /= (pagesize / stepsize);
+	for(address = addrstart; address < addrend; address += stepsize){
+		uint32_t offset;
+		offset = lseek(fd, address, SEEK_SET);
+
+		if (offset != address){
+			char buffer[BUFFER_SIZE];
+			sprintf(buffer, "seeking to offset %uld using index"
+					" %uld", offset, address);
+			handle_errno(buffer);
+		}
+		read(fd, &bitbuffer, sizeof(uint64_t));
+		handle_errno("reading from file for pagemap");
+		if ((bitbuffer & PAGEPRESENT) && !(bitbuffer & PAGESWAPPED)){
+			uint64_t pfn = bitbuffer & PFNBITS;
+			struct pagedata *pData = g_hash_table_lookup(pages,
+				&pfn);
+			if (pData){
+				pData->procmapped += 1;
+			}
+		}
+	}
+
 }
 
 void lookup_other_PIDs(const pid_t *procs, uint proccount, GHashTable *pages){ /*Reminder: Currently not called anywhere*/
 	FILE* filemaps;
 	int i;
+
+	if(DEBUG)
+		printf("In function lookup_other_PIDs\n");
+
 	for(i = 1; i < proccount; i++){
+
 		char pathbuf[PATH_MAX];
 		char buffer[BUFFER_SIZE];
 		pid_t PID = procs[i];
 		int fdpagemap;
+
 		sprintf(pathbuf, "/proc/%d/maps", PID);
 		filemaps = fopen(pathbuf, "r");
 		handle_errno("opening maps file");
@@ -164,7 +276,7 @@ struct sizestats *stats)
 	uint64_t index;/*Number of bytes to seek to in pageflags*/
 	uint64_t offset;/*The position in the file that seek reaches*/
 	uint64_t bitbuffer;/*64 bits to be read and interpreted*/
-
+	size_t pagesize = getpagesize();
 	index = PFN*8;
 	offset = lseek(fdpagecount, index, SEEK_SET);
 	if (offset != index){
@@ -179,7 +291,7 @@ struct sizestats *stats)
 	printf("%d,%Ld|", pData->procmapped, bitbuffer);
 	pData->memmapped = bitbuffer;
 	if (bitbuffer == 1)
-		stats->uss += 1;
+		stats->uss += pagesize;
 }
 
 void lookup_page(uint64_t index, int fdpagemap, GHashTable *pages,
@@ -258,6 +370,18 @@ int fdpagemap, int fdpageflags, int fdpagecount, struct sizestats *stats)
 
 }
 
+void lookup_smaps(pid_t PID, struct sizestats *stats)
+{
+	char buffer[BUFFER_SIZE];
+	FILE *file;
+	sprintf(buffer, "/proc/%d/smaps", PID);
+	file = fopen(buffer, "r");
+	handle_errno("opening smaps file");
+	parse_smaps_file(file, stats);
+	fclose(file);
+	handle_errno("closing smaps file");
+}
+
 int main(int argc, char *argv[])
 {
 	int fdpagemap;
@@ -274,9 +398,16 @@ int main(int argc, char *argv[])
 	GHashTable *pages = g_hash_table_new_full(g_int64_hash,
 		 g_int64_equal, &destroyval, &destroyval);
 	struct sizestats stats;
+		stats.vss = 0;
+		stats.rss = 0;
+		stats.pss = 0;
 		stats.uss = 0;
 		stats.gss = 0;
 		stats.sss = 0;
+		stats.refd = 0;
+		stats.swap = 0;
+		stats.anon = 0;
+		stats.locked = 0;
 
 	if (signal(SIGTERM, cleanup) == SIG_ERR){/*terminate, sent by kill*/
 		fprintf(stderr, "Error occurred setting signal handler\n");
@@ -299,7 +430,8 @@ int main(int argc, char *argv[])
 		PIDs[loopval-1] = strtoul(argv[loopval], NULL, 0);
 		kill(PIDs[loopval-1], SIGSTOP);/*SIGSTOP overrides signal
 						*handling, consider SIGSTP*/
-		printf("Stopping process %s\n", argv[loopval]);
+		if (DEBUG) 
+			printf("Stopping process %s\n", argv[loopval]);
 		handle_errno("stopping process");
 	}/*Assuming only arguments are PIDs: end*/
 
@@ -330,7 +462,7 @@ int main(int argc, char *argv[])
 				" address start and address end\n");
 			exit(EXIT_FAILURE);
 		}
-		pagecount = (addrend - addrstart)/pagesize;
+		pagecount = (addrend - addrstart) / pagesize;
 		printf("VMA is %uld pages long\n", pagecount);
 		if (pagecount < 1){
 			fprintf(stderr, "Error: VMA contains no pages\n");
@@ -350,7 +482,12 @@ int main(int argc, char *argv[])
 	handle_errno("closing maps file");
 
 	g_hash_table_foreach(pages, countsss, &stats);
-
+	lookup_other_PIDs(PIDs, PIDcount, pages);
+	g_hash_table_foreach(pages, countgss, &stats);
+	lookup_smaps(PIDs[0], &stats);
+	stats.uss /= KBSIZE;
+	stats.gss /= KBSIZE;
+	stats.sss /= KBSIZE;
 	printsummary(&stats);
 	cleanup(0);
 	exit(0);
